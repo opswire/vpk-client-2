82d9eca2414897e83371b4468c10bafd8d0dedd7
Stanislav Chikhichin <stchikhichin@gmail.com>
2024-09-23 00:40:09 +0300

    lab3 completed

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,33 +1,22 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '../StoreContext';
 import RecipeCard from './RecipeCard';
 import Pagination from '../helpers/Pagination';
 import catAnimation from '../assets/cat.gif';
-import { fetchRecipes, toggleShowFavorites, setPage } from '../recipeSlice';
 
-const RecipeList = () => {
-    const dispatch = useDispatch()
-    const { 
-        recipes,
-        loading, 
-        error, 
-        showFavorites, 
-        currentPage, 
-        recipesPerPage,
-    } = useSelector(state => state.recipes)
-    
+const RecipeList = observer(() => {
+    const { recipeStore } = useStore();
+    const { recipes, loading, error, showFavorites, currentPage, recipesPerPage } = recipeStore;
+
+    useEffect(() => {
+        recipeStore.fetchRecipes();
+    }, [recipeStore]);
+
     const filteredRecipes = showFavorites ? recipes.filter(recipe => recipe.isFavorite) : recipes;
     const indexOfLastRecipe = currentPage * recipesPerPage;
     const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
     const currentRecipes = filteredRecipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
-
-    const handlePageChange = (pageNumber) => {
-        dispatch(setPage(pageNumber));
-    };
-
-    useEffect(() => {
-        dispatch(fetchRecipes());
-    }, [dispatch])
 
     if (loading) {
         return <p>Загрузка рецептов...</p>;
@@ -40,26 +29,23 @@ const RecipeList = () => {
     return (
         <div>
             <h2 style={{ display: 'flex', alignItems: 'center' }}>
-                Список рецептов: 
-                <img src={catAnimation} alt="Кот" style={{ width: '50px', marginLeft: '10px' }} />
+                Список рецептов:
+                <img src={catAnimation} alt="Котик" style={{ width: '50px', marginLeft: '10px' }} />
             </h2>
-            <button className="filter-btn" onClick={() => dispatch(toggleShowFavorites())}>
+            <button className="filter-btn" onClick={() => recipeStore.toggleShowFavorites()}>
                 {showFavorites ? 'Показать все рецепты' : 'Показать только избранные'}
             </button>
             {currentRecipes.map((recipe) => (
-                <RecipeCard 
-                    key={recipe.id} 
-                    recipe={recipe}
-                />
+                <RecipeCard key={recipe.id} recipe={recipe} />
             ))}
-           <Pagination 
+            <Pagination 
                 totalRecipes={filteredRecipes.length} 
                 recipesPerPage={recipesPerPage} 
-                paginate={handlePageChange}
-                currentPage={currentPage}
+                paginate={(pageNumber) => recipeStore.setPage(pageNumber)} 
+                currentPage={currentPage} 
             />
         </div>
     );
-}
+});
 
 export default RecipeList;
